d2f132117dc376cd961e1345779901b5a60c7c90
joshua.amigogo <joshua.amigogo@gmail.com>
2023-01-27 22:43:18 +0000

    init

---
+++ server/app.js
@@ -0,0 +1,26 @@
+//import modules
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
+
+//app
+const app = express();
+
+//db
+
+
+//middleware
+app.use(morgan("dev"));
+app.use(cors({origin: true, credentials: true}));
+
+//routes
+
+
+
+//ports
+const port = process.env.PORT || 8080;
+
+//listener
+const server = app.listen(port, () => console.log('server is running on port'))
