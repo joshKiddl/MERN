cee27bdc93dacb5613f723ff2183874434acb735
joshua.amigogo <joshua.amigogo@gmail.com>
2023-01-31 17:35:39 +0000

    Working flights manager

--- server/models/workoutModel.js
+++ server/models/workoutModel.js
@@ -3,18 +3,26 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const workoutSchema = new Schema({
-  title: {
+  number: {
     type: String,
     required: true
   },
-  reps: {
+  origin: {
+    type: String,
+    required: true
+  },
+  destination: {
+    type: String,
+    required: true
+  },
+  operator: {
+    type: String,
+    required: true
+  },
+  price: {
     type: Number,
     required: true
   },
-  load: {
-    type: Number,
-    required: true
-  }
 }, { timestamps: true })
 
 module.exports = mongoose.model('Workout', workoutSchema)
